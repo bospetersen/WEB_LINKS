b316dc243a31bb88d081b073257758159e45f922
bospetersen <bo.s.petersen@gmail.com>
2023-03-20 07:02:08 +0100

    added _code

--- AI-Machinelearninng/hardware.md
+++ AI-Machinelearninng/hardware.md
@@ -1,7 +1,6 @@
 # Coral Dev Board
 [Google begins selling the $150 Coral Dev Board, a hardware kit for accelerated AI edge computing](https://venturebeat.com/ai/google-begins-selling-the-150-coral-dev-board-a-hardware-kit-for-accelerated-ai-edge-computing/)
 
-
 ## Coral Dev Board 1GB / 4GB
 * [www.arrow.com](https://www.arrow.com/en/products/g950-06210-01/google-corporation)
 * [pi3g.com](https://pi3g.com/products/machine-learning/google-coral/coral-dev-board-4gb/)
---
+++ __Code/get_files.py
@@ -0,0 +1,7 @@
+url_001 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/hardware.md"
+url_002 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/communicating_sharing_work.md"
+url_003 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/machinelearning.md"
+
+urls = [{"name": 'Hardware', "url": url_001}, 
+        {"name": 'Communicating sharing work', "url": url_002}, 
+        {"name": 'Software/Machinelearning', "url": url_003}]
