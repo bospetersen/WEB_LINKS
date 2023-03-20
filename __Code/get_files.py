d5d7ca6cf2fe883bfb382b43c536acb65bb63004
bospetersen <bo.s.petersen@gmail.com>
2023-03-20 12:19:44 +0100

    test: new filenames

--- __Code/get_files.py
+++ __Code/get_files.py
@@ -1,7 +1,25 @@
-url_001 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/hardware.md"
-url_002 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/communicating_sharing_work.md"
-url_003 = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/machinelearning.md"
+root_path = "https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main"
+urls = [
+"https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/hardware.md",
+"https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/Communicating sharing work.md",
+"https://raw.githubusercontent.com/bospetersen/WEB_LINKS/main/AI-Machinelearninng/machinelearning.md"
+]
 
-urls = [{"name": 'Hardware', "url": url_001}, 
-        {"name": 'Communicating sharing work', "url": url_002}, 
-        {"name": 'Software/Machinelearning', "url": url_003}]
+def make_select_name(root_path, url):
+        select_path = url.split(root_path) 
+        select_path = select_path[1][1:]
+        select_path = select_path[:-3]
+        return select_path
+
+
+select_names = []
+for url in urls:
+        name = make_select_name(root_path, url)
+        select_names.append(name)
+
+
+print(select_names)
+
+# urls = [{"name": 'Hardware', "url": url_001}, 
+#         {"name": 'Communicating sharing work', "url": url_002}, 
+#         {"name": 'Software/Machinelearning', "url": url_003}]
