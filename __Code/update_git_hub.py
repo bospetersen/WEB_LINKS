c2ce28565b6abb4e937e51a5158e2fb52977464e
bospetersen <bo.s.petersen@gmail.com>
2023-03-30 16:56:57 +0200

    Updated files: 2023/03/30 16:56:57

--- __Code/update_git_hub.py
+++ __Code/update_git_hub.py
@@ -7,7 +7,7 @@ today =format(time.strftime("%Y/%m/%d %H:%M:%S"))
 print(today)
 
 os.system(f'cd {os.path.dirname(__file__)}../')
-os.system(f'git add . && git commit -m"Update files: {today}" && git push origin main')
+os.system(f'git add . && git commit -m"Updated files: {today}" && git push origin main')
 
 #  git add . && git commit -m"Update files" && git push origin main
 # os.system(f'cd  && git add . && git commit -m"Update files" && git push origin main')
