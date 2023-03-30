e8886d8b5b3d12e718b64d64ac07b866b26c2b74
bospetersen <bo.s.petersen@gmail.com>
2023-03-30 13:22:21 +0200

    Update files

---
+++ __Code/update_git_hub.py
@@ -0,0 +1,7 @@
+import os
+
+
+os.system(f'cd {os.path.dirname(__file__)}../ && git add . && git commit -m"Update files" && git push origin main')
+
+#  git add . && git commit -m"Update files" && git push origin main
+# os.system(f'cd  && git add . && git commit -m"Update files" && git push origin main')
