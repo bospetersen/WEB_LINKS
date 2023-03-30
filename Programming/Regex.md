7c6656b2c1fc7ddaf2d667975ef25a9ef8f2a1ac
bospetersen <bo.s.petersen@gmail.com>
2023-03-30 16:05:21 +0200

    Update files

--- Programming/Regex.md
+++ Programming/Regex.md
@@ -2,3 +2,6 @@
 # Regular Expressions
 [Regular Expressions Cheat Sheet](https://cheatography.com/davechild/cheat-sheets/regular-expressions/)
 [regexone.com](https://regexone.com/)
+
+## Video
+[Use ChatGPT to Build a RegEx Generator – OpenAI API Low Code Course](https://www.youtube.com/watch?v=D6Xj_W4leu8)
--- __Code/update_git_hub.py
+++ __Code/update_git_hub.py
@@ -1,7 +1,8 @@
 import os
 
 
-os.system(f'cd {os.path.dirname(__file__)}../ && git add . && git commit -m"Update files" && git push origin main')
+os.system(f'cd {os.path.dirname(__file__)}../')
+os.system(f'git add . && git commit -m"Update files" && git push origin main')
 
 #  git add . && git commit -m"Update files" && git push origin main
 # os.system(f'cd  && git add . && git commit -m"Update files" && git push origin main')
